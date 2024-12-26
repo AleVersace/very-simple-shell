cec369f71500ae936df86b940ee2c2c125592dfd
Alessandro Versace <aleversace98@gmail.com>
2024-12-26 15:28:25 +0100

    feat(shell): basic command parsing with invalid check

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,5 +10,19 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+	}
+
+	parseCommand(command)
+}
+
+func parseCommand(command string) {
+	switch command {
+	case "echo":
+	case "cd":
+	default:
+		fmt.Printf("%s command not found\n", command)
+	}
 }
