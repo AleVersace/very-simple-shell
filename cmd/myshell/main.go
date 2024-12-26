2f6a0bbe284694da27aa97651b634d9ecf92e27d
Alessandro Versace <aleversace98@gmail.com>
2024-12-26 15:45:23 +0100

    feat(exit): implement basic exit command parsing

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -18,15 +19,31 @@ func main() {
 		}
 		command = strings.TrimSpace(command)
 
-		parseCommand(command)
+		programArgs := strings.Split(command, " ")
+		program := programArgs[0]
+		programArgs = programArgs[1:]
+		parseCommand(program, programArgs)
 	}
 }
 
-func parseCommand(command string) {
+func parseCommand(command string, args []string) {
 	switch command {
 	case "echo":
 	case "cd":
+	case "exit":
+		exit(args)
 	default:
 		fmt.Printf("%s: command not found\n", command)
 	}
+}
+
+func exit(args []string) {
+	if len(args) == 0 || len(args) > 1 {
+		os.Exit(0)
+	}
+	exitCode, err := strconv.ParseInt(args[0], 10, 32)
+	if err != nil {
+		os.Exit(0)
+	}
+	os.Exit(int(exitCode))
 }
